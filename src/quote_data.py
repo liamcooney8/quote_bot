34e35ad765edc4c105ad5e368a6c35046cdde69e
Liam Cooney <cooneylb@bc.edu>
2021-11-24 00:48:46 -0500

    first commit

---
+++ src/quote_data.py
@@ -0,0 +1,17 @@
+import csv
+import random
+import pickle
+
+with open("quote_df.csv", mode="r") as df:
+    reader = csv.reader(df)
+    df_dict = {rows[0]:rows[1] for rows in reader}
+
+
+def choose_quote(data):
+    while True:
+        quote, author = random.choice(list(data.items()))
+        check_quote = f"'{quote}' - {author}"
+        if len(check_quote) < 280:
+            return check_quote
+
+
---
+++ src/quote_tweeter.py
@@ -0,0 +1,29 @@
+import tweepy
+import os
+from dotenv import load_dotenv
+from quote_data import choose_quote, df_dict
+from time import sleep
+
+load_dotenv()
+
+bearer_token = os.getenv("BEARER_TOKEN")
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_key = os.getenv("ACCESS_KEY")
+access_secret = os.getenv("ACCESS_SECRET")
+
+client = tweepy.Client(bearer_token=bearer_token, 
+                        consumer_key=consumer_key, 
+                        consumer_secret=consumer_secret,
+                        access_token=access_key,
+                        access_token_secret=access_secret)
+
+
+count = 0 
+while count < 10:
+    tweet_info = choose_quote(df_dict)
+    print(f"\nTweet: {tweet_info}")
+    client.create_tweet(text=tweet_info)
+    print("\nSuccessfully Tweeted")
+    count += 1
+    sleep(10)
